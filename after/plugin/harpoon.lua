e292470b10756e713a0fec26659024a6c46aa727
Lucas Bombarda <lucas@devdock.com.br>
2024-05-07 20:19:03 -0300

    feat: added function to delete entry on harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,21 +4,39 @@ harpoon:setup()
 
 -- basic telescope configuration
 local conf = require("telescope.config").values
+
 local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
+    local finder = function()
+        local paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+            table.insert(paths, item.value)
+        end
+
+        return require("telescope.finders").new_table({
+            results = paths,
+        })
     end
 
     require("telescope.pickers").new({}, {
         prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
+        finder = finder(),
         previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
+        sorter = require("telescope.config").values.generic_sorter({}),
+
+        attach_mappings = function(prompt_bufnr, map)
+            map("i", "<C-d>", function()
+                local state = require("telescope.actions.state")
+                local selected_entry = state.get_selected_entry()
+                local current_picker = state.get_current_picker(prompt_bufnr)
+
+                table.remove(harpoon_files.items, selected_entry.index)
+                current_picker:refresh(finder())
+            end)
+            return true
+        end,
     }):find()
 end
+
 
 vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
     { desc = "Open harpoon window" })
