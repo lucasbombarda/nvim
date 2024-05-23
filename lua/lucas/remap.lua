47ab83a8888f0ba368e6761b1a26c24d379f61b2
Lucas Bombarda <lucas@devdock.com.br>
2024-05-23 19:28:13 -0300

    refactor: changed shortcut for accept lsp suggestion

--- lua/lucas/remap.lua
+++ lua/lucas/remap.lua
@@ -33,7 +33,6 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 -- This is going to get me cancelled
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
