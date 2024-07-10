59a587dc20e8f2fc65545a3d5738df64e8ffcf0f
Lucas Bombarda (Note-UL) <lucas@devdock.com.br>
2024-07-10 13:01:31 -0300

    feat: added sql

--- lua/lucas/lazy/lsp.lua
+++ lua/lucas/lazy/lsp.lua
@@ -34,6 +34,7 @@ return {
                 "tailwindcss",
                 "html",
                 "cssls",
+                "sqlls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
