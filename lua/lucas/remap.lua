3d91e74cc6cb5f37f59d5959d76202de90075a43
Lucas Bombarda <lucas@devdock.com.br>
2024-07-24 20:31:48 -0300

    feat: added arrow keys bindings
    I will get used later

--- lua/lucas/remap.lua
+++ lua/lucas/remap.lua
@@ -39,8 +39,3 @@ vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
--- Disable arrow keys to get used to hjkl
-vim.keymap.set({ "n", "v", "i", "c" }, "<Up>", "<nop>")
-vim.keymap.set({ "n", "v", "i", "c" }, "<Down>", "<nop>")
-vim.keymap.set({ "n", "v", "i", "c" }, "<Left>", "<nop>")
-vim.keymap.set({ "n", "v", "i", "c" }, "<Right>", "<nop>")
